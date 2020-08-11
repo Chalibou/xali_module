cf2f22e4f8678cff635d4ae4ceabc08d37395b4e
Chalibou <rom.lemeur@gmail.com>
2020-08-11 13:32:56 -0500

    Write file if does not exists

--- install/install.js
+++ install/install.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 
 console.log('RUNNING');
 
-fs.mkdirSync(`./../..//client`, { recursive: true });
-fs.mkdirSync(`./../../server_data/https`, { recursive: true });
-fs.mkdirSync(`./../..//server_data/jwt`, { recursive: true });
-fs.mkdirSync(`./../..//server_data/mail`, { recursive: true });
-fs.mkdirSync(`./../..//server_data/lang`, { recursive: true });
-fs.mkdirSync(`./../..//server_data/post`, { recursive: true });
+fs.mkdirSync(`./../../client`, { recursive: true });
+fs.mkdirSync(`./../../server/https`, { recursive: true });
+fs.mkdirSync(`./../..//server/jwt`, { recursive: true });
+fs.mkdirSync(`./../..//server/mail`, { recursive: true });
+fs.mkdirSync(`./../..//server/lang`, { recursive: true });
+fs.mkdirSync(`./../..//server/post`, { recursive: true });
 
 console.log('MADE FOLDERS');
 
-fs.writeFile(`./../../server_data/post/post.js`, '//Write here your post methods', function (err) {
+fs.writeFile(`./../../server/post/post.js`, '//Write here your post methods',{ flag: 'wx' }, function (err) {
     if (err) return console.log(err);
     console.log('Created POST mathods file');
 });
