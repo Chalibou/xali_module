41325da48ed85951a13fcf7d668b97cf72eb62f8
Chalibou <rom.lemeur@gmail.com>
2020-08-11 14:09:23 -0500

    key generation for jwt

--- install/install.js
+++ install/install.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const crypto = require("crypto")
 
-console.log('RUNNING');
+console.log( `[XALI]--RUNNING XALI'S SCRIPT`);
 
 fs.mkdirSync(`./../../client`, { recursive: true });
 fs.mkdirSync(`./../../server/https`, { recursive: true });
@@ -9,11 +10,41 @@ fs.mkdirSync(`./../..//server/mail`, { recursive: true });
 fs.mkdirSync(`./../..//server/lang`, { recursive: true });
 fs.mkdirSync(`./../..//server/post`, { recursive: true });
 
-console.log('MADE FOLDERS');
-
-fs.writeFile(`./../../server/post/post.js`, '//Write here your post methods',{ flag: 'wx' }, function (err) {
+const callback = (err)=>{
     if (err) return console.log(err);
-    console.log('Created POST mathods file');
-});
+    return;
+}
 
-console.log('MADE FILE');
+fs.writeFile(`./../../server/post/post.js`, '//Write here your post methods',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/https/server.cert`, '',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/https/server.key`, '',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/lang/en-EN.json`, '//Write your parameters as "param1":"This is some text thazt will replace the HTML beacon #{param1}"',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/lang/es-ES.key`, '//Inscribe su parametros como "param1":"Eso es untexto que va remplazar la baliza HTML #{param1}"',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/mail/dkim_private.key`, '',{ flag: 'wx' },callback);
+fs.writeFile(`./../../server/mail/dkim_public.key`, '',{ flag: 'wx' },callback);
+
+//Generate RSA KEY PAIR for JWT
+crypto.generateKeyPair('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'super top secret'
+    }
+    },
+    (err, publicKey, privateKey) => {
+        if (err) {
+            return console.log(err);
+        }
+        // Handle errors and use the generated key pair.
+        fs.writeFile(`./../../server/jwt/private.key`,privateKey,{ flag: 'wx' },callback);
+        fs.writeFile(`./../../server/jwt/public.key`,publicKey,{ flag: 'wx' },callback);
+
+        console.log('[XALI]--SCRIPT END');
+    }
+);
