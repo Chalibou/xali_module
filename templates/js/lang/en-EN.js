e3d3390f656ea76e43381293880837fdc07735cf
Chalibou <rom.lemeur@gmail.com>
2020-08-20 11:46:47 -0500

    salutations !

--- templates/js/lang/en-EN.js
+++ templates/js/lang/en-EN.js
@@ -3,4 +3,6 @@ toolText = {
     ,register_success:()=>'Registration is sucessfull'
     ,wrong_user:(user)=>`Username ${user} is not registered in this platform`
     ,wrong_key:()=>"The password you have entered is not correct"
+    ,low_accreditation:()=>"Your credential level does not allow you to access this request"
+    ,hi:(name)=>`Hi ${name} how are you today ?`
 }
--- templates/js/lang/es-ES.js
+++ templates/js/lang/es-ES.js
@@ -3,4 +3,6 @@ toolText = {
     ,register_success:()=>'Registracion exitosa'
     ,wrong_user:(user)=>`Usuario ${user} no es registrado en esa plataforma`
     ,wrong_key:()=>"La clave entrada no es correcta"
+    ,low_accreditation:()=>"Su nivel de autorizacion no le permite acceder a esa queja"
+    ,hi:(name)=>`Hola ${name} ¿ Como esta hoy ?`
 }
