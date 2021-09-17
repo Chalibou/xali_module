8822381676f69fda640e576307c8c3455e0a2990
Chalibou <rom.lemeur@gmail.com>
2021-09-17 15:22:41 -0500

    small error

--- local_modules/payu.js
+++ local_modules/payu.js
@@ -16,8 +16,6 @@ class payu{
     setup = (options)=>{
         this.merchandId = options.merchandId;
         this.accountId = options.accountId;
-        this.confirmationUrl = options.confirmationUrl;
-        this.responseUrl = options.responseUrl;
         this.privateK = options.privateK;
 		this.payuUrl = "https:///checkout.payulatam.com/ppp-web-gateway-payu/";
 		this.logger.success("PAYU","Setup",`Payment system set-up, sandbox is ${options.isSandbox}`);
@@ -38,8 +36,6 @@ class payu{
 			cart.tax = this.tax;
 			cart.taxReturnBase = this.taxReturnBase;
 			cart.test = 0;
-			cart.responseUrl = this.responseUrl;
-			cart.confirmationUrl = this.confirmationUrl;
 			cart.action = this.payuUrl;
 			try{
 				cart.signature = crypto.createHash('md5').update(`${this.privateK}~${this.merchandId}~${cart.referenceCode}~${cart.amount}~${cart.currency}`).digest("hex");
